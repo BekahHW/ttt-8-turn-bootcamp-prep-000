f376b4cd5b339bc6cdc25d051f6b90be39713e33
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-02-08 10:08:31 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+ 
+def input_to_index(index)
+ user_input = index.to_i-1
+end
+
+def valid_move?(board, index)
+index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+def move(board, index, player)
+  board[index] = player;
+ 
+ 
+
+end
+
+
+def turn(board)
+   puts "Please enter 1-9:";
+input=gets.strip
+  
+ 
+end
+
+  
